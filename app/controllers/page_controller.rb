80fd82ad39645bd9bb3f1d330e7861b003743aef
André Oviedo <aroviedom@gmail.com>
2024-10-30 19:10:19 +0000

    final work

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,22 @@
+class PageController < ApplicationController
+  def dice 
+    @num_dice = params.fetch("number_of_dice").to_i
+
+    @sides = params.fetch("how_many_sides").to_i
+
+    @rolls = []
+
+    @num_dice.times do
+    die = rand(1..@sides)
+
+    @rolls.push(die)
+    end
+
+    render({:template => "dice/flexible"})
+  end
+  
+  def home 
+  
+    render({:template => "dice/home"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get("/dice/:number_of_dice/:how_many_sides", { :controller => "page", :action => "dice" })
 
+  get("/", { :controller => "page", :action => "home" })
 end
